0cec23ec11b92556904d7828e0550517553bccd2
Carina Brücher <cbr@koempf24.de>
2021-05-20 11:01:28 +0200

    Adds crund cycle and displays data for recepies

--- src/app/recepie/recepie.component.ts
+++ src/app/recepie/recepie.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { RecepieService} from '../recepie.service';
+import { RecepieModel} from '../recepie-model';
 import { AddNewRecepieComponent } from './../add-new-recepie/add-new-recepie.component';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import {TestModel} from '../test-model';
@@ -12,10 +14,21 @@ import {map} from 'rxjs/operators';
   styleUrls: ['./recepie.component.scss'],
 })
 export class RecepieComponent implements OnInit {
+  Recepies: RecepieModel[];
 
-  constructor() { }
+  constructor(private recepieService: RecepieService) { }
 
-  ngOnInit(): void {
+  @Input() recepietitle: string; picture: string; categorie: string; diet: string; difficulty: string; ingredients: string; nutrition: string; preparation: string; time: string;
+  ngOnInit() {
+    this.recepieService.getRecepieList().subscribe(res => {
+      this.Recepies = res.map( e => {
+        console.log(e.payload.doc.data() as object);
+        return {
+          id: e.payload.doc.id,
+          ...(e.payload.doc.data() as object)
+        } as RecepieModel;
+      });
+    });
   }
 }
 
