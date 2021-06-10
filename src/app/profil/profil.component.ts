5d079b3d93ed5ad0b401df9b51501e15645c6b54
Leeniy <marleen@lh-net.de>
2021-06-10 14:28:14 +0200

    Logout

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-profil',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
 
-  constructor() { }
+  constructor( public firebaseService: FirebaseService ) { }
 
   ngOnInit(): void {
   }
+
+  toLogout(){
+    return this.firebaseService.logout();
+   }
 
 }
